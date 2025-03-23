7b606ccf5fd4c8eeae0a9b70bd745bbcb348aa1f
Rushia1810 <wongguanhao05@gmail.com>
2025-03-23 19:20:41 +0800

    Update Homepage.java
    change homepage to fit user functions

--- Javaproject/src/Homepage.java
+++ Javaproject/src/Homepage.java
@@ -3,25 +3,10 @@ import java.util.Scanner;
 public class Homepage {
     
     public static void main(String[] args) {
-        usermenu();
+        User user = new User();
+        user.usermenu();
                 
           
     }
-    public static void usermenu(){
-        Scanner scanner = new Scanner(System.in);
-        int choice = 0;
-        System.out.println("this is a menu");
-        System.out.println("1.register");
-        System.out.println("2.login");
-        System.out.print("Enter a number: ");
-        choice = scanner.nextInt();
-        switch(choice){
-            case 1:
-            User.register();
-            break;
-            case 2:
-            System.out.println("login");;
-            break;            
-        } scanner.close();
-    }
+    
 }
